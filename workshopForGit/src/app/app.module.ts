45082303a9d0e922c2ea452162b071e3c3756892
Lucia Feddersen <luciafeddersen@MBP-von-Lucia.fritz.box>
2021-10-20 00:12:31 +0200

    grundgerüst fertig

--- workshopForGit/src/app/app.module.ts
+++ workshopForGit/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './landingPage/navbar.component';
+import { TableComponent } from './table/table.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TableComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
